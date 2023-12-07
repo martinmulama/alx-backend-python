c456e5ec9b74da1dc1c07d1ac7c8655e431f7313
martinmulama <martin.mulama@yahoo.com>
2023-12-07 10:40:12 +0000

    added 6-sum_mixed_list

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 2. Basic annotations - floor
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float 'n' as an argument and returns its floor as an integer.
+
+    Parameters:
+    - n (float): The floating-point number.
+
+    Returns:
+    int: The floor of 'n'.
+    """
+    return math.floor(n)
